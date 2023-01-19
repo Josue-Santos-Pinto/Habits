da12b2c946ee4c0ce7994b686afc724d865b1be7
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-01-19 18:10:21 -0300

    dayHabit && completed Habits created

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
+
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-
-    const habits = await prisma.habit.findMany({
-        where:{
-            title:{
-                startsWith:'E'
-            }
-        }
-    })
-
-    return habits
-})
 
 app.listen({
     port:3333,
